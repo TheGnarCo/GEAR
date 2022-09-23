7a8198213216b1be934dc00fca6b3db5f56361ee
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-09-22 20:20:31 -0400

    use secrets

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, ENV.fetch("GOOGLE_CLIENT_ID", nil), ENV.fetch("GOOGLE_CLIENT_SECRET", nil),
+  config.omniauth :google_oauth2,
+    Rails.application.credentials.google_client_id,
+    Rails.application.credentials.google_client_secret,
     {
       hd: ["gnar.dog", "gnar.co", "gnar.com"],
       scope: "userinfo.email, userinfo.profile",
