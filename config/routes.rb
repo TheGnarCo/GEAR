7abc15ff420d62e8a78c1d2829cff23f19dacc20
Ethan Fertsch <ethan@gnar.dog>
2022-10-11 11:57:56 -0400

    Scope assets resources to gnar
    When attempting to add the endpoint to display a form to create a new `asset`, a `404 Not Found` was returned and `AssetsController#new` was never hit. Upon inspection of the routes, Propshaft was determined to be the culprit (see the first line item).
    
    ```
        Prefix Verb   URI Pattern                Controller#Action
                      /assets                    Propshaft::Server
        assets GET    /assets(.:format)          assets#index
               POST   /assets(.:format)          assets#create
     new_asset GET    /assets/new(.:format)      assets#new
    edit_asset GET    /assets/:id/edit(.:format) assets#edit
         asset GET    /assets/:id(.:format)      assets#show
               PATCH  /assets/:id(.:format)      assets#update
               PUT    /assets/:id(.:format)      assets#update
               DELETE /assets/:id(.:format)      assets#destroy
          root GET    /
    ```
    
    In short, the `/assets` endpoint defined by Propshaft was interfering with the `assets` resources we were attempting to define. Instead of changing the model name and dealing with the ripple effect of doing so, the `assets` resources are now nested under `gnar` which will allow us to keep the data model the same (and maintain the integrity of the G.E.A.R acronym). For example, the route for `Assets#index` will be generated as `/gnar/assets` instead of `/assets`.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "assets#index"
+
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   devise_scope :user do
@@ -6,5 +8,7 @@ Rails.application.routes.draw do
     get "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
   end
 
-  root "assets#index"
+  scope :gnar do
+    resources :assets, only: [:index]
+  end
 end
