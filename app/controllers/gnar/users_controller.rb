99eeeaeb1aa1fcf11e82ce1466ecefdacdaa5e8c
Alex Jarvis <alxjrvs@users.noreply.github.com>
2023-06-29 19:44:55 -0400

    Unify styling (#334)

--- app/controllers/gnar/users_controller.rb
+++ app/controllers/gnar/users_controller.rb
@@ -32,7 +32,7 @@ module Gnar
     private
 
     def user_params
-      params.permit(:first_name, :last_name, :email)
+      params.require(:user).permit(:first_name, :last_name, :email)
     end
 
     def set_users
