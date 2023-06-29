6e13659dbfb146633268b097b7014583495c65ef
Alex Jarvis <alxjrvs@users.noreply.github.com>
2023-06-29 10:00:21 -0400

    Edit Asset (#331)

---
+++ spec/system/update_asset_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+describe "Updating an Asset", js: true do
+  before do
+    user = create(:user, first_name: "Amy")
+    create(:asset, serial_number: "ZZZZ")
+    login_as(user)
+  end
+
+  it "allows an existing asset to be updated" do
+    visit gnar_assets_path
+
+    click_on "Edit"
+    fill_in "Serial number", with: "AAAA"
+
+    click_on "Send it!"
+
+    expect(page).to have_text "AAAA"
+  end
+
+  context "if the data is invalid" do
+    it "displays an error message" do
+      visit gnar_assets_path
+
+      click_on "Edit"
+      fill_in "Serial number", with: ""
+
+      click_on "Send it!"
+
+      expect(page).to have_text("Serial number can't be blank")
+    end
+  end
+end
