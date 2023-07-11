36c9a37ab6a4793f0fb0291b2125dcfa5bb64a99
alxjrvs@gmail.com <alxjrvs@gmail.com>
2023-07-11 14:41:29 -0400

    No need for persist

--- app/controllers/dev_sign_in_controller.rb
+++ app/controllers/dev_sign_in_controller.rb
@@ -2,10 +2,7 @@ class DevSignInController < ApplicationController
   def index
     if Rails.env.development?
       @user = User.first_or_create(email: "JortsCat@gnar.dog")
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-      end
-
+      sign_in_and_redirect @user, event: :authentication
     else
       raise "Can not run in production"
     end
