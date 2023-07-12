5eacd52e8e8dd6c5d76b440653430b60c2e8615c
alxjrvs@gmail.com <alxjrvs@gmail.com>
2023-07-12 08:48:11 -0400

    WIP

--- app/models/gnar/asset.rb
+++ app/models/gnar/asset.rb
@@ -2,6 +2,10 @@ module Gnar
   class Asset < ApplicationRecord
     belongs_to :user
     default_scope { order(:asset_type, :approximate_purchase_date) }
+    after_update_commit :broadcast_updates
+
+    scope :retired, -> { where(retired: true) }
+    scope :active, -> { where(retired: false) }
 
     enum asset_type: {
       laptop: "laptop",
@@ -16,5 +20,16 @@ module Gnar
     validates :model_number, presence: true
     validates :serial_number, presence: true
     validates :approximate_purchase_date, presence: true
+
+    private
+
+    def broadcast_updates
+      Turbo::StreamsChannel.broadcast_replace_to(
+        Gnar::Asset.active.first,
+        target: "gnar-assets",
+        partial: "gnar/assets/table",
+        locals: { assets: Asset.active.all, retired_assets: Asset.retired.all },
+      )
+    end
   end
 end
