9d0e1bcc7a85472cdb7a363d9be4bbc02e1ce5b9
alxjrvs@gmail.com <alxjrvs@gmail.com>
2023-08-30 10:24:06 -0400

    Passing coverage

---
+++ spec/system/retire_user_asset_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+describe "Retire an Asset", js: true do
+  let(:user) { create(:user, first_name: "Amy") }
+
+  before do
+    create(:asset, user:, serial_number: "ZZZZ")
+    login_as(user)
+    visit gnar_user_path(user)
+  end
+
+  it "allows an asset to be retired and unretired" do
+    within("#assets") do
+      expect(page).to have_text("ZZZZ")
+    end
+
+    within("#retired-assets") do
+      expect(page).not_to have_text("ZZZZ")
+    end
+
+    click_on "Retire"
+
+    within("#assets") do
+      expect(page).not_to have_text("ZZZZ")
+    end
+
+    within("#retired-assets") do
+      expect(page).to have_text("ZZZZ")
+    end
+
+    click_on "UnRetire"
+
+    within("#assets") do
+      expect(page).to have_text("ZZZZ")
+    end
+
+    within("#retired-assets") do
+      expect(page).not_to have_text("ZZZZ")
+    end
+  end
+end
